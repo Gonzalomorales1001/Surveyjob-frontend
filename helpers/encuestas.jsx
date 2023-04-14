ecc32ec5e97243bbcb5aa2135a9d345233d0fdd6
SantiagoGit <santiagobaigorria7@gmail.com>
2023-04-14 17:53:25 -0300

    agregamos rama y carpeta herlpers para las funciones de la api

---
+++ helpers/encuestas.jsx
@@ -0,0 +1,12 @@
+const url = "";
+const token = ;
+const limite = ;
+
+//crear encuestas funciones async para esperar respuesta de la api 
+// ver encuestas funciones async para esperar respuesta de la api 
+// editar encuestas funciones async para esperar respuesta de la api 
+// borrar encuestas funciones async para esperar respuesta de la api 
+//editar usuario  funciones async para esperar respuesta de la api 
+//borrar usuarios funciones async para esperar respuesta de la api 
+
+
