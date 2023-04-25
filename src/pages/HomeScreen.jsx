5225732a68c2b9e56ee430c91f7941e0fe42be7c
Gonzalomorales1001 <gonzalomorales1001@gmail.com>
2023-04-25 01:07:01 -0300

    obteniendo los datos de encuesta

---
+++ src/pages/HomeScreen.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Survey from '../components/Survey'
+
+import {GetAllSurveys} from '../helpers/Surveys'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+const HomeScreen = () => {
+
+    const [allSurveys, setAllSurveys] = useState([])
+
+    const getSurveys=()=>{
+        GetAllSurveys().then(r=>setAllSurveys(r.surveys))
+        GetAllSurveys().then(r=>console.log(r.surveys))
+    }
+
+    useEffect(() => {
+      getSurveys()
+    }, [])
+    
+  return (
+    <>
+    <h1 className='text-center'>Lista de encuestas</h1>
+    <hr />
+    <div>  
+        {allSurveys.map((survey,index)=>{
+            return (
+                <Survey category={survey.category} title={survey.title} surveyID={survey.surveyID} key={index}/>
+            )
+        })}
+    </div>
+    </>
+  )
+}
+
+export default HomeScreen
