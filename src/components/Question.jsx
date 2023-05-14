bed29c7cf61a94b1cef7bcbb35f6378ca3f560c3
Gonzalomorales1001 <gonzalomorales1001@gmail.com>
2023-05-13 21:10:45 -0300

    la pagina de encuestas ahora carga las preguntas y tiene nuevos estilos :)

---
+++ src/components/Question.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import '../css/Question.css'
+
+const Question = ({content,questionType,options,questionID,questionNumber,surveyCategory,dark}) => {
+
+    const questionTypeRender=(type)=>{
+        switch(type){
+            case "TEXT":
+                return (
+                    <div className='col'>
+                        <textarea name={`textarea-${questionID}`} id={`textarea-${questionID}`} className={`form-control question__text ${dark&&'question__text--dark'}`}></textarea>
+                    </div>
+                )
+            break
+            case "SELECT":
+            case "RADIO":
+                return options.map((option,index)=>{
+                    return (
+                            <div className="col-12 question__radio mb-2">
+                                <input type="radio" className="btn-check" id={`btn-check-outlined-${questionID}-radio-${index+1}`} name="options-outlined" autoComplete="off"/>
+                                <label className={`btn ${dark?'btn-outline-light':'btn-outline-dark'} w-100 rounded-pill`} htmlFor={`btn-check-outlined-${questionID}-radio-${index+1}`}>{option}</label><br/>
+                            </div>
+                    )
+                })
+            break
+            case "CHECKBOX":
+                return options.map((option,index)=>{
+                        return (
+                            <div className="col-12 col-md-6 question__check mb-2">
+                                <input type="checkbox" className="btn-check" id={`btn-check-outlined-${questionID}-check-${index+1}`} autoComplete="off"/>
+                                <label className={`btn ${dark?'btn-outline-light':'btn-outline-dark'} w-100 rounded-1 px-4 py-2 text-start`} htmlFor={`btn-check-outlined-${questionID}-check-${index+1}`}>{option}</label><br/>
+                            </div>
+                        )
+                    })
+            break
+        }
+    }
+
+  return (
+    <div className="container p-0 p-md-auto px-lg-5">
+        <div className="row">
+            <div className="col p-0 p-md-auto px-lg-5">
+                <div className={`card card-margin w-100 ${dark&&'card--dark text-light'}`}>
+                    <div className="card-body pt-4">
+                        <div className="widget-49">
+                            <div className={`widget-49-title-wrapper`}>
+                                <div className={`flex-shrink-0 ${dark?'widget-49-date-light':'widget-49-date-dark'}`}>
+                                    <span className="widget-49-date-day fs-1">{questionNumber}</span>
+                                </div>
+                                <div className="widget-49-meeting-info">
+                                    <span className={`widget-49-pro-title fs-2 ${dark&&'text-light'}`}>{content}</span>
+                                </div>
+                            </div>
+                            <br />
+                            <div className="row justify-content-start">
+                                {questionTypeRender(questionType)}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Question
+
+
+
+
+                            {/* <ol className="widget-49-meeting-points">
+                                <li className="widget-49-meeting-item"><span>Expand module is removed</span></li>
+                                <li className="widget-49-meeting-item"><span>Data migration is in scope</span></li>
+                                <li className="widget-49-meeting-item"><span>Session timeout increase to 30 minutes</span></li>
+                            </ol> */}
+                            {/* <div className="widget-49-meeting-action">
+                                <a href="#" className="btn btn-sm btn-flash-border-primary">View All</a>
+                            </div> */}
--- src/components/SpeechBubble.jsx
+++ src/components/SpeechBubble.jsx
@@ -4,7 +4,7 @@ import '../css/SpeechBubble.css'
 
 const SpeechBubble = ({position,direction,color,flip,body,textLight}) => {
   return (
-    <div style={{"--bbColor":color}} className={`speech-bubble p${position} a${direction}  mx-auto`}>
+    <div style={{"--bbColor":color}} className={`speech-bubble p${position} a${direction} ${flip&&'flip'} mx-auto`}>
         {/* <div className="title">{title}</div> */}
         <p className={`${textLight===true&&'text-light'} fw-100 speech-bubble__text`}>{body}</p>
     </div>
