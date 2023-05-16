e5cf313f8bcd42bcaa21f26748c47cd454d062b8
SantiagoGit <santiagobaigorria7@gmail.com>
2023-05-15 21:25:07 -0300

    admin maquetado faltan comprobar las funciones

--- src/components/CardUsuer.jsx
+++ src/components/CardUsuer.jsx
@@ -1,64 +1,64 @@
-import React from "react";
-// importar funciones de borrar y editar usuarios
-import Swal from "sweetalert2"; // este es el modal de sweeralert
+// import React from "react";
+// // importar funciones de borrar y editar usuarios
+// import Swal from "sweetalert2"; // este es el modal de sweeralert
 
-const CardUsuer = ({usuario}) => { //traigo los datos desde admin
-    const { username, email, userID, status } = usuario; //datos a usar de los usuarios
+// const CardUsuer = ({usuario}) => { //traigo los datos desde admin
+//     const { username, email, userID, status } = usuario; //datos a usar de los usuarios
 
-const inactivarUsuario = async () => {
-    Swal.fire({
-      // este es el modal de sweeralert
-    title: `Está seguro que quiere inactivar este usuario ${username}?`,
-    showDenyButton: true,
-    showCancelButton: false,
-    confirmButtonText: "Si",
-    denyButtonText: `No`,
-    }).then((result) => {
-    if (result.isConfirmed) {
-        borrarUsuario(userID).then((resultado) => {// agregar funciones
-        Swal.fire("", `${resultado.msg}`, "success");
-        });
-    } else if (result.isDenied) {
-        Swal.fire("El usuario no se inactivó", "", "info");
-    }
-    });
-};
-return (
-    <>
-    <div className="card col-12 my-2">
-        <td>
-        <div className="d-flex justify-content-end gap-3 me-2 mt-2">
-            <button
-            className="btn btn-warning btn-sm"
-              onClick={() => handleShow(userID)} // agregar funciones
-            >
-            <i className="fa fa-pencil" aria-hidden="true"></i>
-            </button>
-            <button
-            className="btn btn-danger btn-sm"
-              onClick={() => inactivarUsuario(username, userID)} // agregar funciones
-            >
-            <i className="fa fa-trash" aria-hidden="true"></i>
-            </button>
-        </div>
-        </td>
-        <div className="row ">
-        <div className="d-flex justify-content-center col-md-4 ">
-            <img className="avatar" src={avatar} alt="avatar" />
-        </div>
-        <div className="col-md-8 ">
-            <div className="card-body">
-            <h5 className="card-title">{username}</h5>
+// const inactivarUsuario = async () => {
+//     Swal.fire({
+//       // este es el modal de sweeralert
+//     title: `Está seguro que quiere inactivar este usuario ${username}?`,
+//     showDenyButton: true,
+//     showCancelButton: false,
+//     confirmButtonText: "Si",
+//     denyButtonText: `No`,
+//     }).then((result) => {
+//     if (result.isConfirmed) {
+//         borrarUsuario(userID).then((resultado) => {// agregar funciones
+//         Swal.fire("", `${resultado.msg}`, "success");
+//         });
+//     } else if (result.isDenied) {
+//         Swal.fire("El usuario no se inactivó", "", "info");
+//     }
+//     });
+// };
+// return (
+//     <>
+//     <div className="card col-12 my-2">
+//         <td>
+//         <div className="d-flex justify-content-end gap-3 me-2 mt-2">
+//             <button
+//             className="btn btn-warning btn-sm"
+//               onClick={() => handleShow(userID)} // agregar funciones
+//             >
+//             <i className="fa fa-pencil" aria-hidden="true"></i>
+//             </button>
+//             <button
+//             className="btn btn-danger btn-sm"
+//               onClick={() => inactivarUsuario(username, userID)} // agregar funciones
+//             >
+//             <i className="fa fa-trash" aria-hidden="true"></i>
+//             </button>
+//         </div>
+//         </td>
+//         <div className="row ">
+//         <div className="d-flex justify-content-center col-md-4 ">
+//             <img className="avatar" src={avatar} alt="avatar" />
+//         </div>
+//         <div className="col-md-8 ">
+//             <div className="card-body">
+//             <h5 className="card-title">{username}</h5>
 
-            <p className="card-text">
-                <small className="text-body-secondary">{email}</small>
-            </p>
-            </div>
-        </div>
-        </div>
-    </div>
-    </>
-);
-};
+//             <p className="card-text">
+//                 <small className="text-body-secondary">{email}</small>
+//             </p>
+//             </div>
+//         </div>
+//         </div>
+//     </div>
+//     </>
+// );
+// };
 
-export default CardUsuer;
+// export default CardUsuer;
--- src/views/AdminsScreen.jsx
+++ src/views/AdminsScreen.jsx
@@ -14,8 +14,10 @@ const AdminsScreen = ({ dark, usersx, company, survey }) => {
   //desestructurar bien los datos para usar en section
 
   const [users, setUsers] = useState([]);
-  const [totalusuarios, setTotalUsuarios] = useState(0);
-  const limite = 3;
+  const [surveys, setSurveys] = useState([]);
+  const [totalEncuestas, setTotalEncuestas] = useState(0);
+  const [totalUsuarios, setTotalUsuarios] = useState(0);
+  const limite = 3; //prueba con limite
   const [pagina, setPagina] = useState(0);
 
   const traerUsuarios = async () => {
@@ -26,71 +28,75 @@ const AdminsScreen = ({ dark, usersx, company, survey }) => {
   useEffect(() => {
     traerUsuarios();
   }, [pagina]);
+
   const traerEncuestas = async () => {
-    const { Users } = await getSurveys(limite, pagina);
-    console.log(Users);
-    setUsers(Users);
+    const { surveys } = await getSurveys(limite, pagina);
+    console.log(surveys);
+    setSurveys(surveys);
   };
   useEffect(() => {
-    traerUsuarios();
+    traerEncuestas();
   }, [pagina]);
 
   () => setPagina((prevPagina) => prevPagina + 1);
   return (
-    <div className={` ${dark ? "secAdmin-dark" : "secAdmin-light"}`}>
-      <div className="container-fluid w-100">
-        <div className="row  py-5">
-          <div className="col text-center ">
-            <h1>
-              <span>
-                <i className="fa fa-cogs" aria-hidden="true"></i>{" "}
-              </span>
-              Bienvenido al tablero administrador
+    <>
+      <div className={` ${dark ? "secAdmin-dark" : "secAdmin-light"}`}>
+        <div className="container-fluid w-100">
+          <div className="row  py-5">
+            <div className="col text-center ">
+              <h1>
+                <span>
+                  <i className="fa fa-cogs" aria-hidden="true"></i>{" "}
+                </span>
+                Bienvenido al tablero administrador
+              </h1>
+            </div>
+          </div>
+          {/* hacer un ternario para que aparezca este mensaje o lo que pido encuestas ousers */}
+          <div className="mb-5 d-flex justify-content-center">
+            {" "}
+            <h1 className="fw-200">
+              Survey Job <img src={Job} width="35px" alt="Job letter" /> La
+              gestión en tus manos
             </h1>
           </div>
-        </div>
-        {/* hacer un ternario para que aparezca este mensaje o lo que pido encuestas ousers */}
-        <div className="mb-5 d-flex justify-content-center">
-          {" "}
-          <h1 className="fw-200">
-            Survey Job <img src={Job} width="35px" alt="Job letter" /> La
-            gestión en tus manos
-          </h1>
-        </div>
           <section className="statistics mt-4 mb-4 mx-4">
             <div className="row d-flex align-items-center justify-content-center">
               {/* <div className="col-md-1 "></div> */}
               <div className="col-lg-4">
-                <div className="box d-flex rounded-2 align-items-center p-3 btn btn-get mb-2">
+                <div
+                  type="button"
+                  className="box d-flex rounded-2 align-items-center p-3 btn-get mb-2"
+                >
                   <i className="uil-file fs-2 text-center bg-danger rounded-circle"></i>
-                  <div
-                    className="ms-3"
-                    type="button"
-                    onClick={() => getSurveys}
-                  >
+                  <div className="ms-3" onClick={() => getSurveys}>
                     <div className="d-flex align-items-center">
-                      <h3 className="mb-0">34</h3>{" "}
+                      <h3 className="mb-0">{totalEncuestas}</h3>{" "}
                       <span className="d-block ms-2">Encuestas</span>
                     </div>
-                    <p className="fs-normal mb-0">Lorem ipsum dolor sit amet</p>
+                    {/* <p className="fs-normal mb-0">Lorem ipsum dolor sit amet</p> */}
                   </div>
                 </div>
               </div>
               <div className="col-lg-4">
-                <div className="box d-flex rounded-2 align-items-center p-3 btn btn-get mb-2">
+                <div
+                  type="button"
+                  className="box d-flex rounded-2 align-items-center p-3  btn-get mb-2"
+                >
                   <i className="uil-users-alt fs-2 text-center bg-success rounded-circle"></i>
-                  <div className="ms-3" type="button" onClick={() => getUsers}>
+                  <div className="ms-3" onClick={() => getUsers}>
                     <div className="d-flex align-items-center">
-                      <h3 className="mb-0">5,245</h3>{" "}
+                      <h3 className="mb-0">{totalUsuarios}</h3>{" "}
                       <span className="d-block ms-2">Usuarios</span>
                     </div>
-                    <p className="fs-normal mb-0">Lorem ipsum dolor sit amet</p>
+                    {/* <p className="fs-normal mb-0">Lorem ipsum dolor sit amet</p> */}
                   </div>
                 </div>
               </div>
             </div>
           </section>
-        {/* <div className="d-flex">
+          {/* <div className="d-flex">
           <div id="sidebar-menu" className="menuAdmin row ">
             <button
               type="button"
@@ -98,8 +104,8 @@ const AdminsScreen = ({ dark, usersx, company, survey }) => {
               onClick={() => getUsers}
             >
               Usuarios */}
-        {/* ver onClick que no estan llamando a las funciones */}
-        {/* </button>
+          {/* ver onClick que no estan llamando a las funciones */}
+          {/* </button>
             <button
               type="button"
               className="btn btn-get mb-2"
@@ -107,39 +113,28 @@ const AdminsScreen = ({ dark, usersx, company, survey }) => {
             >
               Encuestas
             </button>
-          </div>
-          <div className="row col-md-6">
-            <div>
-              {users.length > 0 ? (
-                users.map((users) => <CardUser users={users} />)
-              ) : (
-                <div className="spinner-border text-warning" role="status">
+            </div>*/}
+        </div>
+
+        <div className="row d-flex align-items-center">
+          {/* usar para traerusers  */}
+          <div className="col-12 col-md-8 offset-md-2 mb-5">
+            {users.length > 0 ? (
+              users.map((users) => (
+                // <p>{usuario.username}</p>
+                <CardUser key={users.userID} usuario={users} />
+              ))
+            ) : (
+              <div className="d-flex justify-content-center">
+                <div className="spinner-border text-warning  " role="status">
                   <span className="visually-hidden">Loading...</span>
                 </div>
-              )}
-            </div>
+              </div>
+            )}
           </div>
-        </div> */}
-        {/* usar para traerusers 
-          <div className="row">
-            <div className="col-12 col-md-8 offset-md-2 mb-5">
-              {usuarios.length > 0 ? (
-               users.map((usuario) => (
-                  // <p>{usuario.username}</p>
-                  <CardUser key={usuario.userID} usuario={usuario} />
-                ))
-              ) : (
-                <div className="d-flex justify-content-center">
-                  <div className="">
-                    <span className="">Loading...</span>
-                  </div>
-                </div>
-              )}
-            </div>
-            
-          </div> */}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
