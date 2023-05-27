6a90be39e56fc3a4d69d4adc949d7588dab113d7
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-05-27 19:33:50 -0300

    agregue el carrusel en varsión light

--- src/views/HomeScreen.jsx
+++ src/views/HomeScreen.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
 import SpeechBubble from '../components/SpeechBubble'
 import { UserContext,DarkModeContext } from "../App";
-import HS1 from '../assets/CarruselHS/HS1.jpg'
-import HS2 from '../assets/CarruselHS/HS2.jpg'
-import HS3 from '../assets/CarruselHS/HS3.jpg'
-import HS4 from '../assets/CarruselHS/HS4.jpeg'
+import HS1D from '../assets/CarruselHS/HS1D.jpg'
+import HS2D from '../assets/CarruselHS/HS2D.jpg'
+import HS3D from '../assets/CarruselHS/HS3D.jpg'
+import HS1L from '../assets/CarruselHS/HS1L.jpg'
+import HS2L from '../assets/CarruselHS/HS2L.jpg'
+import HS3L from '../assets/CarruselHS/HS3L.jpg'
 import "../css/HomeScreen.css";
 
 
@@ -27,28 +29,59 @@ const HomeScreen = () => {
        <div className="carousel-inner">
          <div className="carousel-item active">
            <img
-             src={HS1}
+             src={HS1D}
              className="d-block w-100"
              alt="imagen 1"
            />
          </div>
          <div className="carousel-item">
            <img
-             src={HS2}
+             src={HS2D}
              className="d-block w-100"
              alt="imagen 2"
            />
          </div>
          <div className="carousel-item">
            <img
-             src={HS3}
+             src={HS3D}
              className="d-block w-100"
              alt="imagen 3"
            />
          </div>
+         <div className="overlaycarrusel">
+           <div className="container h-100">
+             <div className="row h-100 align-items-center">
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </section>
+   <section id="carruselImg">
+     <div
+       id="carousel"
+       className="carousel slide carousel-fade"
+       data-bs-ride="carousel"
+       data-bs-pause="off"
+     >
+       <div className="carousel-inner">
+         <div className="carousel-item active">
+           <img
+             src={HS1L}
+             className="d-block w-100"
+             alt="imagen 1"
+           />
+         </div>
          <div className="carousel-item">
            <img
-             src={HS4}
+             src={HS2L}
+             className="d-block w-100"
+             alt="imagen 2"
+           />
+         </div>
+         <div className="carousel-item">
+           <img
+             src={HS3L}
              className="d-block w-100"
              alt="imagen 3"
            />
