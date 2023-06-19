9b678b16c908678af4f1d78f003a15eb6d945246
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-06-19 19:13:59 -0300

    cambio pagenorfound con estados blanco y oscuro

--- src/views/PageNotFoundScreen.jsx
+++ src/views/PageNotFoundScreen.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { UserContext, DarkModeContext } from "../App";
+import errorL from '../assets/error404L.jpg'
+import errorD from '../assets/error404D.png'
+
+
 
 const PageNotFoundScreen = () => {
+  const {dark}=useContext(DarkModeContext)
+
   return (
-    <>PageNotFoundScreen</>
+    <img src={dark ? errorD : errorL } alt="error 404" />
   )
 }
 
