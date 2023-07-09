05a3b1471ed269db0385c427c8e6c7ebac1a2027
Gonzalomorales1001 <gonzalomorales1001@gmail.com>
2023-07-09 00:04:14 -0300

    homescreen empezado

--- src/views/HomeScreen.jsx
+++ src/views/HomeScreen.jsx
@@ -7,94 +7,72 @@ import HS3D from '../assets/CarruselHS/HS3D.jpg'
 import HS1L from '../assets/CarruselHS/HS1L.jpg'
 import HS2L from '../assets/CarruselHS/HS2L.jpg'
 import HS3L from '../assets/CarruselHS/HS3L.jpg'
+import LOGO_light from '../assets/DarkLetterLogo.png'
+import LOGO_dark from '../assets/LightLetterLogo.png'
+import survey from '../assets/survey.svg'
 import "../css/HomeScreen.css";
+import { Carousel } from 'react-bootstrap';
 
 
 const HomeScreen = () => {
   const {dark}=useContext(DarkModeContext)
 
   return (
-    <>
-    <section className={`${dark&&'bg-dark text-light'}`}>
-      <h1>Bienvenido a SurveyJob</h1>
-    </section>
-    <section id="carruselImg">
-     <div
-       id="carousel"
-       className="carousel slide carousel-fade"
-       data-bs-ride="carousel"
-       data-bs-pause="off"
-     >
-       <div className="carousel-inner">
-         <div className="carousel-item active">
-           <img
-             src={HS1D}
-             className="d-block w-100"
-             alt="imagen 1"
-           />
-         </div>
-         <div className="carousel-item">
-           <img
-             src={HS2D}
-             className="d-block w-100"
-             alt="imagen 2"
-           />
-         </div>
-         <div className="carousel-item">
-           <img
-             src={HS3D}
-             className="d-block w-100"
-             alt="imagen 3"
-           />
-         </div>
-         <div className="overlaycarrusel">
-           <div className="container h-100">
-             <div className="row h-100 align-items-center">
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>
-   </section>
-   <section id="carruselImg">
-     <div
-       id="carousel"
-       className="carousel slide carousel-fade"
-       data-bs-ride="carousel"
-       data-bs-pause="off"
-     >
-       <div className="carousel-inner">
-         <div className="carousel-item active">
-           <img
-             src={HS1L}
-             className="d-block w-100"
-             alt="imagen 1"
-           />
-         </div>
-         <div className="carousel-item">
-           <img
-             src={HS2L}
-             className="d-block w-100"
-             alt="imagen 2"
-           />
-         </div>
-         <div className="carousel-item">
-           <img
-             src={HS3L}
-             className="d-block w-100"
-             alt="imagen 3"
-           />
-         </div>
-         <div className="overlaycarrusel">
-           <div className="container h-100">
-             <div className="row h-100 align-items-center">
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>
-   </section>
-    </>
+    <main className={`${dark?'texturized--dark text-light':'texturized--light'} pb-3`}>
+      <div className="container mb-3">
+        <div className="row row-cols-1 row-cols-lg-2">
+          <div className="col text-center d-flex justify-content-center align-items-center flex-column">
+            <h1>¿Qué es SurveyJob?</h1>
+            <p>SurveyJob es un servicio de encuestas digitales en el que podrás crear tus propias encuestas y enviar un enlace para que puedan ser respondidas.</p>
+            <p>Es muy sencillo de utilizar y ¡Solo lleva un par de minutos!</p>
+            <button className="btn btn-outline-warning rounded-pill">Comenzar</button>
+          </div>
+          <div className="col d-flex justify-content-center align-items-center">
+            <img src={survey} alt="survey" className="w-100" />
+          </div>
+        </div>
+      </div>
+      <div className="black-overlay my-2 py-3">
+        <div className="container">
+        <div className="row row-cols-1 row-cols-lg-2">
+        <div className="col">
+          <Carousel slide={true} interval={3000} prevIcon={false} nextIcon={false} indicatorLabels={null}>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={dark?HS1D:HS1L}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={dark?HS2D:HS2L}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={dark?HS3D:HS3L}
+                alt="Third slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div className="col d-flex justify-content-center align-content-start align-items-center flex-column text-center pt-3 pt-lg-0">
+          <h2>¿Por qué SurveyJob?</h2>
+          <p>Porque además de hacer encuestas para tus usuarios, ¡También puedes hacer encuestas públicas para que responda la comunidad de SurveyJob!</p>
+        </div>
+      </div>
+        </div>
+      </div>
+      <article>
+        <header>
+          {/* <img className='mx-auto' src={dark?LOGO_dark:LOGO_light} alt="SurveyJob logo" /> */}
+        </header>
+      </article>
+    </main>
   )
 }
 
